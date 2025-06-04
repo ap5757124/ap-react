91b1928751ab5adb2c9acf8aa1ddb053c9fb18ec
ap <593801712@qq.com>
2025-06-04 16:32:03 +0800

    UseStateExample，useEffectExample

--- src/pages/HookExample/components/UseEffectExample.tsx
+++ src/pages/HookExample/components/UseEffectExample.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 
 const useEffectExample: React.FC = () => {
-    return  <div>
-        <div>
-            useEffectExample
-        </div>
-    </div>
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        console.log('组件挂载或更新');
+
+        // 相当于 componentDidMount 和 componentDidUpdate
+        const timerID = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+
+        // 相当于 componentWillUnmount
+        return () => {
+            console.log('清理副作用或组件卸载');
+            clearInterval(timerID);
+        };
+    }, []); // 空依赖数组等同于仅在挂载时执行
+
+    useEffect(() => {
+        console.log('仅在date变化时执行');
+        document.title = `当前时间: ${date.toLocaleTimeString()}`;
+    }, [date]); // 仅在date变化时执行
+
+    return <div>当前时间: {date.toLocaleTimeString()}</div>;
 }
 
 export default useEffectExample;
--- src/pages/HookExample/components/UseStateExample.tsx
+++ src/pages/HookExample/components/UseStateExample.tsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from 'antd';
 
+/**
+ * useState 接收一个参数，即状态的初始值，然后返回一个数组，其中包含两个元素：当前的状态值和一个更新该状态的函数
+ * 在React中你需要将数组视为只读的，不可以直接修改原数组，例如：不可以调用 arr.push() arr.pop() 等方法。
+ * React useState是异步更新的 多次以相同的操作更新状态时，React 会进行比较，如果值相同，则会屏蔽后续的更新行为。自带防抖的功能，防止频繁的更新。可以向set方法传递一个更新函数，而不是一个状态去解决这个问题
+ */
 
 const UseStateExample: React.FC = () => {
-    return <div>
+    const [count, setCount] = useState(0);
+
+    const handleCount = () => {
+        setCount(count + 1);
+        setCount(count + 1);
+        setCount(count + 1);
+    }
+    const handleCount2 = () => {
+        setCount(count => count + 1);
+        setCount(count => count + 1);
+        setCount(count => count + 1);
+    }
+
+    return (
         <div>
-            useStateExample
+            <div>
+                <div>{ count }</div>
+                <Button type="primary" onClick={handleCount}>累加3错误</Button>
+                <Button type="primary" onClick={handleCount2}>累加3正确</Button>
+            </div>
         </div>
-    </div>
+    )
 }
 
 export default UseStateExample;
